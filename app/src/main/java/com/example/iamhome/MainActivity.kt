467492798dff83c8230ce250c2a8dfbcdcf39015
Yura Tr <povel3516@gmail.com>
2023-02-26 23:07:00 +0200

    add new Activity

--- app/src/main/java/com/example/iamhome/MainActivity.kt
+++ app/src/main/java/com/example/iamhome/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.iamhome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openRegister(view: View)
+    {
+        val randomIntent = Intent(this, RegisterWin::class.java)
+
+        startActivity(randomIntent)
     }
 }
