6ca013b18b0897a5c0a441d161fb7a7629eb3a57
Yura Tr <povel3516@gmail.com>
2023-02-28 21:53:02 +0200

    Add userInfo

--- app/src/main/java/com/example/iamhome/RegisterWin.kt
+++ app/src/main/java/com/example/iamhome/RegisterWin.kt
@@ -1,11 +1,31 @@
 package com.example.iamhome
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class RegisterWin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_win)
+    }
+
+    fun readData(view: View)
+    {
+        val fieldName = findViewById<EditText>(R.id.textName);
+        val fieldPassword = findViewById<EditText>(R.id.textPassword);
+        val fieldCheckPassword = findViewById<EditText>(R.id.textPassword2);
+        val fieldEmail = findViewById<EditText>(R.id.textEmail);
+
+        val userName = fieldName.text;
+        val userPassword = fieldPassword.text;
+        val userCheckPassword = fieldCheckPassword.text;
+        val userEmail = fieldEmail.text;
+
+
     }
 }
